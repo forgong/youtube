d01281b855875e1898349be6f5bf604e4447c032
forgong <qeqe3939@naver.com>
2020-06-08 22:08:24 +0900

    제어문부터 post방식까지

--- Nodejs/web2-nodejs/main.js
+++ Nodejs/web2-nodejs/main.js
@@ -1,44 +1,99 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
+
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    <a href="/create">create</a>
+    ${body}
+  </body>
+  </html>
+  `
+}
+function templateList(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i++;
+  }
+  list = list + '</ul>';
+  return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    console.log(queryData.id);
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(url == '/favicon.ico'){
+    var pathname = url.parse(_url, true).pathname;
+    // console.log(url.parse(_url, true));
+    
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        fs.readdir('./data', function(err, filelist){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          response.writeHead(200);
+          response.end(template);
+        })
+      } else {
+          fs.readdir('./data', function(err, filelist){
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+              var title = queryData.id;
+              var list = templateList(filelist);
+              var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+              response.writeHead(200);
+              response.end(template);
+            });
+          });
+      }  
+    } else if(pathname === '/create'){
+      fs.readdir('./data', function(err, filelist){
+        var title = 'WEB - create';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, 
+          `<form action="http://localhost:3000/create_process" method="post">
+              <p><input type="text" name="title" placeholder="title"></p>
+              <p>
+                  <textarea name="description" placeholder="description"></textarea>
+              </p>
+              <p>
+                  <input type="submit">
+              </p>
+           </form>`
+           );
+        response.writeHead(200);
+        response.end(template);
+      })
+    } else if(pathname === '/create_process'){
+        var body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description
+            console.log(post);
+        });
+        response.writeHead(200);
+        response.end('success');
+    } 
+    else {
         response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `
-    response.end(template);
- 
-});
-    })
-    // console.log(__dirname + _url);
+        response.end('Not found');
+      }
+})
     
 app.listen(3000);
